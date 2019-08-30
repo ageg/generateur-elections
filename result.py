325dd5c208a3ff0111262d1c97c66126fafab457
Tommy Savaria <tommy.savaria@protonmail.ch>
2019-08-29 22:06:31 -0400

    Fix result.py

--- result.py
+++ result.py
@@ -3,6 +3,8 @@ from pandas import read_csv
 
 results = read_csv("example/results.csv")
 
+res = {}
+
 l = len(results.values)
 ponderation = [4, 2, 1]
 
@@ -11,8 +13,12 @@ from functools import cmp_to_key
 def cmp_scores(a, b):
     if a[1]["points"] > b[1]["points"]:
         return 1
-    if a[1]["positions"][0] > b[1]["positions"][0]:
+    elif a[1]["points"] < b[1]["points"]:
+        return -1
+    elif a[1]["positions"][1] > b[1]["positions"][1]:
         return 1
+    elif a[1]["positions"][1] < b[1]["positions"][1]:
+        return -1
     
     return -1
 
@@ -20,26 +26,35 @@ def cmp_print(a, b):
     print(a[1], b[1])
     return 0
 
-for x in range(0, 19):
-    score = {}
-    
-    c = 4 + (x*3)
+cols = len(results.columns)
+
+for c in range(7, cols):
     end = results.columns[c].find('?')
     poste = results.columns[c][28:end]
+    cl = results.columns[c].find('Classement')
+    classement = int(results.columns[c][cl+11])
+    points = ponderation[classement-1]
 
     for reponse in results.values:
-        for z in range(0, 3):
-            personne = reponse[c+z]
-            if isinstance(personne, str): 
-                if not personne in score:
-                    score[personne] = { "points": 0, "positions": [0, 0, 0] }
-                    
-                score[personne]["positions"][z] += 1
-                score[personne]["points"]  += ponderation[z]
+        value = reponse[c]
 
-    scoreTuples = sorted(score.items(), key=cmp_to_key(cmp_scores), reverse=True)
+        if isinstance (value, float):
+            continue
+        
+        if(not poste in res):
+            res[poste] = {}
+        
+        if not value in res[poste]:
+            res[poste][value] = {
+                "points": 0,
+                "positions": {1: 0, 2: 0, 3: 0}
+            }
+        
+        res[poste][value]["points"] += points
+        res[poste][value]["positions"][classement] += 1       
+
+for poste in res:
     print(poste)
-    for key, value in scoreTuples:
-        print("{key}: {points} ({p0}, {p1}, {p2})".format(key=key, points=value["points"], p0=value["positions"][0], p1=value["positions"][1], p2=value["positions"][2]))
-    
-    print()
+    scoreTuples = sorted(res[poste].items(), key=cmp_to_key(cmp_scores), reverse=True)
+    for score in scoreTuples:
+        print(score)
